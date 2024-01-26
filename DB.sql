69985970be3e72054c8f2d23b302b4dfaa33bc2b
Gabriel Reinoso <gabriel.reinoso.1996@gmail.com>
2024-01-26 09:45:11 -0300

    Modelo de datos

--- DB.sql
+++ DB.sql
@@ -1,10 +1,41 @@
 CREATE TABLE `userParking` (
-  `iduser` int NOT NULL,
+  `iduser` int NOT NULL AUTO_INCREMENT,
   `mail` varchar(30) DEFAULT NULL,
   `pass` char(60) DEFAULT NULL,
   `nivel` int DEFAULT NULL,
   PRIMARY KEY (`iduser`),
   UNIQUE KEY `mail_UNIQUE` (`mail`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO masgps.userParking (mail, pass, nivel) VALUES ('admin@wit.la', '$2a$12$.RUbsTcGPqOXPUaSjOw/.ON9ZSo629LN.3uPPH3Lsp9lVJ11AokO6', 10);
+INSERT INTO `userParking`
+(`mail`,
+`pass`,
+`nivel`)
+VALUES
+('admin@wit.la',
+'$2a$12$.RUbsTcGPqOXPUaSjOw/.ON9ZSo629LN.3uPPH3Lsp9lVJ11AokO6',
+10);
+
+CREATE TABLE `empParking` (
+  `idemp` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) DEFAULT NULL,
+  `representante` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idemp`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `empParking`
+(`nombre`,
+`representante`)
+VALUES
+('Wit',
+'Administrador');
+
+CREATE TABLE `wlParking` (
+  `idwl` int NOT NULL AUTO_INCREMENT,
+  `patente` varchar(20) DEFAULT NULL,
+  `empresa` int DEFAULT NULL,
+  PRIMARY KEY (`idwl`),
+  UNIQUE KEY `patente_UNIQUE` (`patente`),
+  KEY `fk_wlParking_emp_idx` (`empresa`),
+  CONSTRAINT `fk_wlParking_emp` FOREIGN KEY (`empresa`) REFERENCES `empParking` (`idemp`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ php/whitelist/delete.php
@@ -0,0 +1,57 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Permitir solicitudes desde cualquier origen
+header("Access-Control-Allow-Methods: POST, OPTIONS"); // Permitir solicitudes POST y OPTIONS
+
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    // El navegador está realizando una solicitud de pre-vuelo OPTIONS
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Max-Age: 86400'); // Cache preflight request for 1 day
+    header("HTTP/1.1 200 OK");
+    exit;
+}
+
+include("../conf.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $json_data = file_get_contents("php://input");
+
+    $data = json_decode($json_data, true);
+
+    if ($data !== null){
+        // Obtener datos desde JSON
+        $id = $data["id"];
+
+        $chck = $conn->prepare("SELECT patente FROM wlParking WHERE patente = ?");
+        $chck->bind_param("s",$patente);
+        $chck->execute();
+        $result = $chck->get_result();
+
+        if($result->num_rows == 0){
+            // SQL Seguro
+            $stmt = $conn->prepare("INSERT INTO wlParking (patente, empresa) VALUES (?, ?)");
+            $stmt->bind_param("si", $patente, $empresa);
+    
+            if($stmt->execute()){
+                $id = $conn->insert_id;
+                header('Content-Type: application/json');
+                echo json_encode($id);
+            } else {
+                header('Content-Type: application/json');
+                echo json_encode("Error: " . $conn->error);
+            }
+        } else {
+            header('Content-Type: application/json');
+            echo json_encode(false);
+        }
+
+        $conn->close();
+    } else {
+        http_response_code(400);
+        echo $data;
+        echo "Error al decodificar JSON";
+    }
+} else {
+    http_response_code(405);
+    echo "Solicitud no permitida";
+}
+?>

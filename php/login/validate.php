81b33ac802cdead5cfa0d1e77126c4a09b157b56
Gabriel Reinoso <gabriel.reinoso.1996@gmail.com>
2024-04-10 17:07:41 -0400

    test

--- php/login/validate.php
+++ php/login/validate.php
@@ -12,42 +12,11 @@ if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
     exit;
 }
 
-use Firebase\JWT\JWT; // Importar la clase JWT desde la biblioteca Firebase
-
 try {
     require_once('../../vendor/autoload.php'); // Cargar la biblioteca JWT
     require_once('../conf.php'); // Incluir el archivo de configuración de la base de datos
-    
-    $headers = apache_request_headers(); // Obtener los encabezados de la solicitud
-    
-    // Verificar si el encabezado Authorization contiene un token JWT
-    if (! preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)) {
-        header('HTTP/1.0 400 Bad Request');
-        echo json_encode(['error' => 'No se envió un token']);
-        exit;
-    }
-    
-    $jwt = $matches[1]; // Obtener el token JWT
-    
-    if (! $jwt) {
-        header('HTTP/1.0 400 Bad Request');
-        exit;
-    }
-    
-    $token = JWT::decode($jwt, $secretkey, array('HS256')); // Decodificar el token JWT utilizando la clave secreta
-
-    $now = new DateTimeImmutable(); // Obtener la fecha y hora actual
-    $serverName = "wit.la"; // Nombre del servidor
-    
-    // Verificar la validez del token JWT
-    if ($token->iss !== $serverName ||
-        $token->nbf > $now->getTimestamp() ||
-        $token->exp < $now->getTimestamp())
-    {
-        header('HTTP/1.1 401 Unauthorized'); // Devolver un código de error de autorización si el token no es válido
-        exit;
-    }
-    
+    include('../auth.php');
+        
     // Construir los datos de respuesta con la información del usuario autenticado
     $data = array(["logon" => true,
              "user" => $token->user,

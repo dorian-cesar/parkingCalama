bb1535159f62952ad842777b971e882cf96a4109
adolfosa <adolfo.sepulveda.astroza@gmail.com>
2025-02-08 12:24:17 -0300

    Se adapta función calcAndenes para usar parámetros desde valores.js en lugar de PHP

--- js/Data/valores.js
+++ js/Data/valores.js
@@ -3,4 +3,11 @@ var tarifas = {
     valorMinuto: 30, 
     topeDiario: 300        
   };
+
+// Configuración para Andén
+var configuracion = {
+    nacional: 20,  
+    internacional: 30, 
+    iva: 0.19 
+};
   
